1ea92cae5d013df10d199b9181e3446d362e03a5
Tun Sopheak <sopheaktun.tsp@gmail.com>
2026-08-30 21:01:16 +0700

    appy SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://tun-sopheak-portfolio.vercel.app',
+  integrations: [
+    sitemap(),
+  ],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
